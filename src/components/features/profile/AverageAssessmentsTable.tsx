25fcf1c4281becee3ed92ef3eb49962a12dc1d30
Aleksey Chebykin <chebykin.aleksey2@wb.ru>
2025-08-23 18:07:33 +0300

    ptp-algoritm: part 7 show which compentency was improved

--- src/components/features/profile/AverageAssessmentsTable.tsx
+++ src/components/features/profile/AverageAssessmentsTable.tsx
@@ -1,11 +1,13 @@
+import { TDevelopment } from '@/app/api/exercises/route'
 import { COMPETENCIES, CompetencyCode } from '@/types/assessment'
 
 type TProps = {
   pilotName: string
   competencyAverages: Record<CompetencyCode, number | null>
+  development?: TDevelopment
 }
 
-export const AverageAssessmentsTable = ({ pilotName, competencyAverages }: TProps) => {
+export const AverageAssessmentsTable = ({ pilotName, competencyAverages, development }: TProps) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow mb-6">
       <h3 className="text-xl font-semibold mb-4">Общая таблица компетенций {pilotName}</h3>
@@ -30,6 +32,12 @@ export const AverageAssessmentsTable = ({ pilotName, competencyAverages }: TProp
                   </td>
                   <td className="border border-gray-300 px-4 py-2 text-center font-bold">
                     {competencyAverages[competencyCode]}
+                    {development?.[competencyCode] && (
+                      <span className="text-xs text-green-500">
+                        {' '}
+                        + {development[competencyCode]}
+                      </span>
+                    )}
                   </td>
                 </tr>
               )
