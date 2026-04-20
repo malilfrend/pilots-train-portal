c188688c3b7770a868320ff1e7632451e7c45db7
Алексей Чебыкин <alekseychebykin@MacBook-Air-Irina.local>
2026-04-20 19:15:37 +0300

    add execution time to sessions page

--- src/components/features/profile/AverageAssessmentsTable.tsx
+++ src/components/features/profile/AverageAssessmentsTable.tsx
@@ -8,14 +8,14 @@ type TProps = {
 export const AverageAssessmentsTable = ({ pilotName, competencyScores }: TProps) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow mb-6">
-      <h3 className="text-xl font-semibold mb-4">Общая таблица компетенций {pilotName}</h3>
+      <h3 className="text-xl font-semibold mb-4">{pilotName}</h3>
 
       <div className="overflow-x-auto">
         <table className="min-w-full border-collapse border border-gray-300">
           <thead>
             <tr className="bg-gray-100">
               <th className="border border-gray-300 px-4 py-2 text-left">Компетенция</th>
-              <th className="border border-gray-300 px-4 py-2 text-center">Средняя оценка</th>
+              <th className="border border-gray-300 px-4 py-2 text-center">Оценка</th>
             </tr>
           </thead>
           <tbody>
